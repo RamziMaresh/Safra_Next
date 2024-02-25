7bb2ba29e3f6af3e6ad9efc64d1327fbf59e159b
ramzi <ramzi_maresh@outlook..com>
2024-02-25 15:33:36 +0300

    about

--- app/components/about/AboutSectionOne.tsx
+++ app/components/about/AboutSectionOne.tsx
@@ -15,7 +15,6 @@ const AboutSectionOne = () => {
                         height="254"
                         viewBox="0 0 287 254"
                         fill="none"
-                    //xmlns="http://www.w3.org/2000/svg"
                     >
                         <path
                             opacity="0.2"
