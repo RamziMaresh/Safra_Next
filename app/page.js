73d63da4f16d9b1b70696903196bd782bae5a2c3
ramzi <ramzi_maresh@outlook..com>
2024-02-25 13:30:22 +0300

    about update

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import Hero from "./components/hero/Hero";
-import About from "./pages/about/page";
 export default function Home() {
   return (
     <>
--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -1,15 +1,21 @@
+import AboutSectionOne from "../../components/about/AboutSectionOne";
+import AboutSectionTwo from "../../components/about/AboutSectionTwo";
 
-const About = () => {
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us ! | Safra Co. Ltd.",
+  description: "",
+  // other metadata
+};
+
+const AboutPage = () => {
   return (
     <>
-      <section
-        id="about"
-        className="relative z-10 overflow-hidden bg-white pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
-      >
-        <h1 className="text-2xl text-center p-10">About Page</h1>
-        <h4 className="text-2xl text-center pb-5 text-red-500 font-bold">Under Processing...</h4>
-      </section>
+      <AboutSectionOne />
+      <AboutSectionTwo />
     </>
-  )
-}
-export default About
+  );
+};
+
+export default AboutPage;
