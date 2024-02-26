ffbe606a293d44eb7e017b31c217a69768912d45
ramzi <ramzi_maresh@outlook..com>
2024-02-26 17:46:33 +0300

    aaqq

--- app/pages/products/aliphatic/page.tsx
+++ app/pages/products/aliphatic/page.tsx
@@ -96,7 +96,7 @@ const Aliphatic = () => {
             <div className="w-full px-4 lg:w-8/12">
               <div>
                 <h1 className="mb-4 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight tracking-wide">
-                  Aliphatic Solvents | SPP'S
+                  Aliphatic Solvents | 01
                 </h1>
                 <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
                   <div className="flex flex-wrap items-center">
