7807edf5b0df3a98ae213b650ad0fff918473143
ramzi <ramzi_maresh@outlook..com>
2024-02-26 17:44:48 +0300

    aaaaaa

--- app/pages/products/aliphatic/page.tsx
+++ app/pages/products/aliphatic/page.tsx
@@ -18,79 +18,79 @@ const Aliphatic = () => {
         id="product-alipa"
         className="pb-[120px] pt-[180px] relative z-10 overflow-hidden  dark:bg-gray-dark"
       >
-            <div>
-                <span className="absolute left-0 top-0 z-[-1]">
-                    <svg
-                        width="287"
-                        height="254"
-                        viewBox="0 0 287 254"
-                        fill="none"
-                    >
-                        <path
-                            opacity="0.2"
-                            d="M286.5 0.5L-14.5 254.5V69.5L286.5 0.5Z"
-                            fill="url(#paint0_linear_111:578)"
-                        />
-                        <defs>
-                            <linearGradient
-                                id="paint0_linear_111:578"
-                                x1="-40.5"
-                                y1="117"
-                                x2="301.926"
-                                y2="-97.1485"
-                                gradientUnits="userSpaceOnUse"
-                            >
-                                <stop stopColor="rgb(45 212 191)" />
-                                <stop offset="1" stopColor="rgb(94 234 212)" stopOpacity="0" />
-                            </linearGradient>
-                        </defs>
-                    </svg>
-                </span>
-                <span className="absolute right-0 top-0 z-[-1]">
-                    <svg
-                        width="628"
-                        height="258"
-                        viewBox="0 0 628 258"
-                        fill="none"
-                    //xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            opacity="0.3"
-                            d="M669.125 257.002L345.875 31.9983L524.571 -15.8832L669.125 257.002Z"
-                            fill="url(#paint0_linear_0:1)"
-                        />
-                        <path
-                            opacity="0.2"
-                            d="M0.0716344 182.78L101.988 -15.0769L142.154 81.4093L0.0716344 182.78Z"
-                            fill="url(#paint1_linear_0:1)"
-                        />
-                        <defs>
-                            <linearGradient
-                                id="paint0_linear_0:1"
-                                x1="644"
-                                y1="221"
-                                x2="429.946"
-                                y2="37.0429"
-                                gradientUnits="userSpaceOnUse"
-                            >
-                                <stop stopColor="rgb(45 212 191)" />
-                                <stop offset="1" stopColor="rgb(94 234 212)" stopOpacity="0" />
-                            </linearGradient>
-                            <linearGradient
-                                id="paint1_linear_0:1"
-                                x1="18.3648"
-                                y1="166.016"
-                                x2="105.377"
-                                y2="32.3398"
-                                gradientUnits="userSpaceOnUse"
-                            >
-                                <stop stopColor="rgb(45 212 191)" />
-                                <stop offset="1" stopColor="rgb(94 234 212)" stopOpacity="0" />
-                            </linearGradient>
-                        </defs>
-                    </svg>
-                </span>
-            </div>
+        <div>
+          <span className="absolute left-0 top-0 z-[-1]">
+            <svg
+              width="287"
+              height="254"
+              viewBox="0 0 287 254"
+              fill="none"
+            >
+              <path
+                opacity="0.2"
+                d="M286.5 0.5L-14.5 254.5V69.5L286.5 0.5Z"
+                fill="url(#paint0_linear_111:578)"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_111:578"
+                  x1="-40.5"
+                  y1="117"
+                  x2="301.926"
+                  y2="-97.1485"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="rgb(45 212 191)" />
+                  <stop offset="1" stopColor="rgb(94 234 212)" stopOpacity="0" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+          <span className="absolute right-0 top-0 z-[-1]">
+            <svg
+              width="628"
+              height="258"
+              viewBox="0 0 628 258"
+              fill="none"
+            //xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                opacity="0.3"
+                d="M669.125 257.002L345.875 31.9983L524.571 -15.8832L669.125 257.002Z"
+                fill="url(#paint0_linear_0:1)"
+              />
+              <path
+                opacity="0.2"
+                d="M0.0716344 182.78L101.988 -15.0769L142.154 81.4093L0.0716344 182.78Z"
+                fill="url(#paint1_linear_0:1)"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_0:1"
+                  x1="644"
+                  y1="221"
+                  x2="429.946"
+                  y2="37.0429"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="rgb(45 212 191)" />
+                  <stop offset="1" stopColor="rgb(94 234 212)" stopOpacity="0" />
+                </linearGradient>
+                <linearGradient
+                  id="paint1_linear_0:1"
+                  x1="18.3648"
+                  y1="166.016"
+                  x2="105.377"
+                  y2="32.3398"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="rgb(45 212 191)" />
+                  <stop offset="1" stopColor="rgb(94 234 212)" stopOpacity="0" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+        </div>
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4 lg:w-8/12">
@@ -138,11 +138,11 @@ const Aliphatic = () => {
                   <p className="mb-10 text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed lg:text-base lg:leading-relaxed xl:text-lg xl:leading-relaxed">
                     Safra aliphatic are one of the main stream solvents.
                     These come mainly from oil streams purely by distillation.
-                    Diagrammatized products are more highly refined materials which are virtually "aromatic free"
+                    Diagrammatized products are more highly refined materials which are virtually aromatic free
                     and made by additional chemical treatment, or by chemical processes, achieving lower
                     toxicity and very low odor. Safra provides its customers many aliphatic solvents
-                    both generic and customized even on customers'
-                    own specifications. Few of Safra generic aliphatic solvents are mentioned below:
+                    both generic and customized even on customers own specifications.
+                    Few of Safra generic aliphatic solvents are mentioned below:
                   </p>
                   <div className="mb-10 w-full overflow-hidden rounded">
                     <div className="relative aspect-[97/60] w-full sm:aspect-[97/44]">
