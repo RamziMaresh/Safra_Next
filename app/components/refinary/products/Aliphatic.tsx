e8d336990353b54682bf9682501ff28ec46d3eac
ramzi <ramzi_maresh@outlook..com>
2024-03-03 15:08:31 +0300

    aaa

--- app/components/refinary/products/Aliphatic.tsx
+++ app/components/refinary/products/Aliphatic.tsx
@@ -7,15 +7,19 @@ const Aliphatic = () => {
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full">
+
             <div>
-              <h3 className="font-xl mb-8 uppercase	text-yellow-700 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+              <h3 className="font-xl mb-8 uppercase	tracking-wide text-yellow-700 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
                 Aliphatic Solvents
               </h3>
+
+
               <h3 className="font-xl mb-2 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
-                1- Safranorm™ (Aliphatic Solvent with 20% Vol.)
+                1- Safranorm™<span className="text-lg"> [Normal White Spirite 20%]</span>
               </h3>
               <p className="mb-8 text-base font-medium italic text-body-color">PDF Download: Specs and MSDS </p>
               <div className="grid-container grid grid-cols-5 mb-10">
+
                 <div className="relative item2 col-span-2 w-2/3 bg-gradient-to-r from-teal-600 to-teal-800 rounded-md hidden lg:block">
                   <Image
                     src="/assets/spp/pine.gif"
@@ -24,30 +28,227 @@ const Aliphatic = () => {
                     className="h-auto w-1/2 object-contain object-center p-6"
                   />
                 </div>
-
-
                 <div className="item1 col-span-3">
                   <ul className="list-inside list-disc text-body-color">
                     <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
-                      Optimum Solution viscosities for long-oil alkyd resins & varnishes.
+                      Optimum Solution viscosities for
                     </li>
                     <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
-                      Good cleaning of tar spots. Excellent for care care, Polishes and Waxes.
+                      Optimum Solution viscosities for
                     </li>
                     <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
-                      Milder Odor.
+                      Optimum Solution viscosities for
                     </li>
                     <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
-                      More refined than kerosene.
+                      Optimum Solution viscosities for
                     </li>
                     <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
-                      Good cleaning power for metal and equipment degreasing and preserve wood.
+                      Optimum Solution viscosities for
                     </li>
                   </ul>
                 </div>
               </div>
-
             </div>
+
+            <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
+            </div>
+            <div>
+              <h3 className="font-xl mb-2 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                2- Safranorm 15™ <span className="text-lg"> [Normal White Spirite with 15% ]</span>
+              </h3>
+              <p className="mb-8 text-base font-medium italic text-body-color">PDF Download: Specs and MSDS </p>
+              <div className="grid-container grid grid-cols-5 mb-10">
+
+                <div className="relative item2 col-span-2 w-2/3 bg-gradient-to-r from-teal-600 to-teal-800 rounded-md hidden lg:block">
+                  <Image
+                    src="/assets/spp/pine.gif"
+                    alt="image"
+                    fill
+                    className="h-auto w-1/2 object-contain object-center p-6"
+                  />
+                </div>
+                <div className="item1 col-span-3">
+                  <ul className="list-inside list-disc text-body-color">
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/** */}
+            <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
+            </div>
+            <div>
+              <h3 className="font-xl mb-2 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                3- Safralite™<span className="text-lg"> [Lite White Spirit]</span>
+              </h3>
+              <p className="mb-8 text-base font-medium italic text-body-color">PDF Download: Specs and MSDS </p>
+              <div className="grid-container grid grid-cols-5 mb-10">
+
+                <div className="relative item2 col-span-2 w-2/3 bg-gradient-to-r from-teal-600 to-teal-800 rounded-md hidden lg:block">
+                  <Image
+                    src="/assets/spp/pine.gif"
+                    alt="image"
+                    fill
+                    className="h-auto w-1/2 object-contain object-center p-6"
+                  />
+                </div>
+                <div className="item1 col-span-3">
+                  <ul className="list-inside list-disc text-body-color">
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+              {/** */}
+              <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
+            </div>
+            <div>
+              <h3 className="font-xl mb-2 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                4- Safrasol D40™<span className="text-lg"> [De-Aromatized White Spirit]</span>
+              </h3>
+              <p className="mb-8 text-base font-medium italic text-body-color">PDF Download: Specs and MSDS </p>
+              <div className="grid-container grid grid-cols-5 mb-10">
+
+                <div className="relative item2 col-span-2 w-2/3 bg-gradient-to-r from-teal-600 to-teal-800 rounded-md hidden lg:block">
+                  <Image
+                    src="/assets/spp/pine.gif"
+                    alt="image"
+                    fill
+                    className="h-auto w-1/2 object-contain object-center p-6"
+                  />
+                </div>
+                <div className="item1 col-span-3">
+                  <ul className="list-inside list-disc text-body-color">
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+              {/** */}
+              <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
+            </div>
+            <div>
+              <h3 className="font-xl mb-2 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                5- Safrasol D80™<span className="text-lg"> [De-Aromatized Kerosene]</span>
+              </h3>
+              <p className="mb-8 text-base font-medium italic text-body-color">PDF Download: Specs and MSDS </p>
+              <div className="grid-container grid grid-cols-5 mb-10">
+
+                <div className="relative item2 col-span-2 w-2/3 bg-gradient-to-r from-teal-600 to-teal-800 rounded-md hidden lg:block">
+                  <Image
+                    src="/assets/spp/pine.gif"
+                    alt="image"
+                    fill
+                    className="h-auto w-1/2 object-contain object-center p-6"
+                  />
+                </div>
+                <div className="item1 col-span-3">
+                  <ul className="list-inside list-disc text-body-color">
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+              {/** */}
+              <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
+            </div>
+            <div>
+              <h3 className="font-xl mb-2 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                6- Safrasol 80™<span className="text-lg"> [Heavy White Spirit]</span>
+              </h3>
+              <p className="mb-8 text-base font-medium italic text-body-color">PDF Download: Specs and MSDS </p>
+              <div className="grid-container grid grid-cols-5 mb-10">
+
+                <div className="relative item2 col-span-2 w-2/3 bg-gradient-to-r from-teal-600 to-teal-800 rounded-md hidden lg:block">
+                  <Image
+                    src="/assets/spp/pine.gif"
+                    alt="image"
+                    fill
+                    className="h-auto w-1/2 object-contain object-center p-6"
+                  />
+                </div>
+                <div className="item1 col-span-3">
+                  <ul className="list-inside list-disc text-body-color">
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                    <li className="mb-1 text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                      Optimum Solution viscosities for
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
