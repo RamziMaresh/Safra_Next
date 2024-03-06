085068a50f4702475b25f0d28ce1a1b831345d76
ramzi <ramzi_maresh@outlook..com>
2024-03-06 13:02:22 +0300

    qwqw

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <section
         id="home"
         className="relative z-10 overflow-hidden pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] 
-        xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]
+        xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px] 
         "
       >
 
@@ -23,7 +23,7 @@ const Hero = () => {
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight tracking-wider ">
                   Solutions For Better Living
                 </h1>
-                <p className="bg-gray-200 p-2 rounded tracking-wide	mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+                <p className="rounded tracking-wide	mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
                   Safra is a leading producer and distributor of Hydrocarbon solvents and Chemicals.
                   Our environmental friendly solvents comply with the highest international quality and
                   safety standards. Our fully integrated production, sales & distribution ...
