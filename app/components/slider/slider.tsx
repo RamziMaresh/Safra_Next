e22424b70143d2c8822afe1ee0220d7b8ea4e8e9
ramzi <ramzi_maresh@outlook..com>
2024-03-07 10:28:23 +0300

    qaqa

--- app/components/slider/slider.tsx
+++ app/components/slider/slider.tsx
@@ -9,12 +9,12 @@ export default function Slider() {
 
   return (
     <div
-      className="overflow-hidden bg-gray-300 mb-8 rounded-lg"
+      className="overflow-hidden bg-gray-200 mb-8 rounded-lg"
       ref={emblaRef}>
       <div className="flex">
         {sliderData?.map((item) => {
           return (
-            <div className="embla__slide relative p-2 w-full" key={item.id}>
+            <div className="embla__slide relative p-1 w-full" key={item.id}>
 
               {/* the image */}
               <img className="w-full h-full " src={item.Image} alt="" />
