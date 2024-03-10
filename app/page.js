ddae3172615ad215a310118e80ad7447d7f07b15
ramzi <ramzi_maresh@outlook..com>
2024-03-10 17:13:33 +0300

    qq

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Partners from "./components/hero/Feature";
 import Hero from "./components/hero/Hero";
 import HomeSecTwo from "./components/hero/HomeSecTwo";
 export default function Home() {
