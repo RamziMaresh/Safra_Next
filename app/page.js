38bec61d8dc76cc35104cacf43cb614b6f011242
ramzi <ramzi_maresh@outlook..com>
2024-03-14 14:59:42 +0300

    qqa

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Solutions from "./components/hero/SolutionsFor";
 import Hero from "./components/hero/Hero";
 import HomeSecTwo from "./components/hero/HomeSecTwo";
-import MandV from "./components/hero/MandV";
 export default function Home() {
   return (
     <>
