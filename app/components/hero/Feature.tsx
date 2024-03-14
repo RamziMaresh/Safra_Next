7dd226340919d8412d1035dff143ac4806204c62
ramzi <ramzi_maresh@outlook..com>
2024-03-14 12:31:20 +0300

    www

--- app/components/hero/Feature.tsx
+++ app/components/hero/Feature.tsx
@@ -48,11 +48,13 @@ const Solutions = () => {
 
     return (
         <div className="overflow-hidden pb-24 dark:bg-gray-dark" id='partners'>
-            <div className="px-14 pt-12 space-y-12 pb-12 text-center">
+            <div className="px-14 pt-12 space-y-12 pb-4 text-center">
                 <h2 className="text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
                     Safra Solutions For
                 </h2>
-                <div className="flex justify-center items-center ">
+
+            </div>
+            <div className="flex justify-center items-center mb-12 px-14 ">
                     <Image
                         src=
                         {"/assets/safralines.png"}
@@ -70,8 +72,6 @@ const Solutions = () => {
                         className="mx-auto hidden max-w-full drop-shadow-three dark:block dark:drop-shadow-none lg:mr-0"
                     />
                 </div>
-            </div>
-
             <Slider {...settings} >
                 {
                     partners.map((partner) => {
