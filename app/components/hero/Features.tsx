415563a3237952575c878d00e797ef9dd08ed042
ramzi <ramzi_maresh@outlook..com>
2024-03-16 02:56:52 +0300

    qaq

--- app/components/hero/Features.tsx
+++ app/components/hero/Features.tsx
@@ -2,21 +2,25 @@ import { HiShare, HiDeviceMobile, HiCash, HiLightningBolt, HiBadgeCheck } from '
 
 const features = [
     {
+        id: 1,
         icon: <HiBadgeCheck />,
         name: 'Logistics',
         desc: 'Our full-fieldged logistics department is an integral part of our commitment to meet customers requests and to ensuring prompt deliveries.'
     },
     {
+        id: 2,
         icon: <HiShare />,
         name: 'production facility',
         desc: 'We believe in continuously renovating and upgrading our equipment and tools to guarantee uninterrupted production levels and the efficient distribution of our products across the globe.'
     },
     {
+        id: 3,
         icon: <HiDeviceMobile />,
         name: 'Marketing Relationship Managment',
         desc: 'The relationships developed with our customers have contributed greatly to our current success; the preservation of such solid relationships is the key to our future grouth.'
     },
     {
+        id: 4,
         icon: <HiDeviceMobile />,
         name: 'Sales',
         desc: 'The continuous growth of our sales in various new locations is an outcome of our integral Safra services and dedication to satisfying our customers needs.'
@@ -30,17 +34,17 @@ const Features = () => {
             <div className="py-12 space-y-12">
                 <div className='flex flex-wrap m-auto w-full justify-center items-center '>
                     {
-                        features.map((feature) =>  {
+                        features.map(feature=> {
                             return (
                                 <div >
-                                    <div key="feature" className='flex flex-col cursor-grab justify-center items-center mb-8 space-y-4 hover:bg-[rgba(0,0,0,.015)] group rounded-3xl duration-200 transition p-6'>
-                                        <div  className="text-5xl text-teal-600 bg-[rgba(0,0,0,.015)] rounded-full p-4 group-hover:bg-[rgba(0,0,0,.0)] group-hover:animate-spin">
+                                    <div className='flex flex-col cursor-grab justify-center items-center mb-8 space-y-4 hover:bg-[rgba(0,0,0,.015)] group rounded-3xl duration-200 transition p-6'>
+                                        <div key={feature.id} className="text-5xl text-teal-600 bg-[rgba(0,0,0,.015)] rounded-full p-4 group-hover:bg-[rgba(0,0,0,.0)] group-hover:animate-spin">
                                             {feature.icon}
                                         </div>
-                                        <h1  className='font-black text-teal-700 uppercase md:w-64'>
+                                        <h1 className='font-black text-teal-700 uppercase md:w-64'>
                                             {feature.name}
                                         </h1>
-                                        <p  className='md:w-80  text-sm pb-4'>
+                                        <p className='md:w-80  text-sm pb-4'>
                                             {feature.desc}
                                         </p>
                                     </div>
