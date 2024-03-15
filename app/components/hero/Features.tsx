79ad4685a3b8f1184bb3fb5c32dbf68c8879b441
ramzi <ramzi_maresh@outlook..com>
2024-03-16 02:59:12 +0300

    aa

--- app/components/hero/Features.tsx
+++ app/components/hero/Features.tsx
@@ -33,12 +33,11 @@ const Features = () => {
         <div id='features' className="flex justify-center items-center text-center bg-gray-50 mb-12">
             <div className="py-12 space-y-12">
                 <div className='flex flex-wrap m-auto w-full justify-center items-center '>
-                    {
-                        features.map(feature=> {
+                    {features.map(feature=> {
                             return (
-                                <div >
+                                <div key={feature.id} >
                                     <div className='flex flex-col cursor-grab justify-center items-center mb-8 space-y-4 hover:bg-[rgba(0,0,0,.015)] group rounded-3xl duration-200 transition p-6'>
-                                        <div key={feature.id} className="text-5xl text-teal-600 bg-[rgba(0,0,0,.015)] rounded-full p-4 group-hover:bg-[rgba(0,0,0,.0)] group-hover:animate-spin">
+                                        <div className="text-5xl text-teal-600 bg-[rgba(0,0,0,.015)] rounded-full p-4 group-hover:bg-[rgba(0,0,0,.0)] group-hover:animate-spin">
                                             {feature.icon}
                                         </div>
                                         <h1 className='font-black text-teal-700 uppercase md:w-64'>
