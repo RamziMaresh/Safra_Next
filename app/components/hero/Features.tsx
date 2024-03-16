0fedcb96c8cd286c1ca334ebd4e6ac5da1c30662
ramzi <ramzi_maresh@outlook..com>
2024-03-16 03:32:00 +0300

    aqa

--- app/components/hero/Features.tsx
+++ app/components/hero/Features.tsx
@@ -1,4 +1,7 @@
-import { HiShare, HiDeviceMobile, HiCash, HiLightningBolt, HiBadgeCheck } from 'react-icons/hi';
+import { HiBadgeCheck } from 'react-icons/hi';
+import { TbCirclesRelation } from "react-icons/tb";
+import { SiSalesforce  } from "react-icons/si";
+import { MdProductionQuantityLimits } from "react-icons/md";
 
 const features = [
     {
@@ -9,28 +12,28 @@ const features = [
     },
     {
         id: 2,
-        icon: <HiShare />,
+        icon: <MdProductionQuantityLimits />,
         name: 'production facility',
         desc: 'We believe in continuously renovating and upgrading our equipment and tools to guarantee uninterrupted production levels and the efficient distribution of our products across the globe.'
     },
     {
         id: 3,
-        icon: <HiDeviceMobile />,
-        name: 'Marketing Relationship Managment',
+        icon: <TbCirclesRelation />,
+        name: 'Relationship Managment',
         desc: 'The relationships developed with our customers have contributed greatly to our current success; the preservation of such solid relationships is the key to our future grouth.'
     },
     {
         id: 4,
-        icon: <HiDeviceMobile />,
+        icon: <SiSalesforce />,
         name: 'Sales',
         desc: 'The continuous growth of our sales in various new locations is an outcome of our integral Safra services and dedication to satisfying our customers needs.'
-    },
+    }, 
 ]
 
 const Features = () => {
 
     return (
-        <div id='features' className="flex justify-center items-center text-center bg-gray-50 mb-12">
+        <div id='features' className="flex justify-center items-center text-center mb-12 bg-gray-50">
             <div className="py-12 space-y-12">
                 <div className='flex flex-wrap m-auto w-full justify-center items-center '>
                     {features.map(feature=> {
@@ -40,10 +43,10 @@ const Features = () => {
                                         <div className="text-5xl text-teal-600 bg-[rgba(0,0,0,.015)] rounded-full p-4 group-hover:bg-[rgba(0,0,0,.0)] group-hover:animate-spin">
                                             {feature.icon}
                                         </div>
-                                        <h1 className='font-black text-teal-700 uppercase md:w-64'>
+                                        <h1 className='font-black text-teal-700 capitalize md:w-64'>
                                             {feature.name}
                                         </h1>
-                                        <p className='md:w-80  text-sm pb-4'>
+                                        <p className='md:w-64 text-sm pb-4'>
                                             {feature.desc}
                                         </p>
                                     </div>
--- app/components/hero/SolutionsFor.tsx
+++ app/components/hero/SolutionsFor.tsx
@@ -48,19 +48,19 @@ const Solutions = () => {
     };
 
     return (
-        <div className="overflow-hidden pb-24 dark:bg-gray-dark" id='partners'>
+        <div className="overflow-hidden pb-24 dark:bg-gray-dark " id='partners'>
 
-            <div className="px-14 pt-12 space-y-12 pb-4 text-center ">
-                <h2 className="text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[35px] tracking-wide uppercase">
-                    Solutions For Better living
+            <div className="px-14 pt-12 space-y-12 pb-4 text-center bg-gray-50">
+                <h2 className="text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[35px] tracking-wide capitalize">
+                    Total Quality Managment
                 </h2>
-
-                <p className="mb-10 capitalize text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed lg:text-base lg:leading-relaxed xl:text-lg xl:leading-relaxed">
-                                        Our product portfolio has been identified as one of our core competencies, giving us a competitive edge in the local, regional and international markets.
-                                    </p>
             </div>
             <Features />
-        
+            <div className="px-14 pt-12 mb-12  pb-4 text-center">
+                <h2 className="text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[35px] tracking-wide capitalize">
+                    Safra Solution 
+                </h2>
+            </div>
             <Slider {...settings} >
                 {
                     partners.map((partner) => {
@@ -78,3 +78,11 @@ const Solutions = () => {
 }
 
 export default Solutions
+
+
+/**
+                
+                <p className="mb-10 capitalize text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed lg:text-base lg:leading-relaxed xl:text-lg xl:leading-relaxed">
+                                        Our product portfolio has been identified as one of our core competencies, giving us a competitive edge in the local, regional and international markets.
+                                    </p>
+                                     */
