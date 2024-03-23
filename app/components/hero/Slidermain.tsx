23d10f21201827ef75746622ad5a7b3d83ae9dbe
ramzi <ramzi_maresh@outlook..com>
2024-03-23 22:28:03 +0300

    qww

--- app/components/hero/Slidermain.tsx
+++ app/components/hero/Slidermain.tsx
@@ -1,20 +1,17 @@
-
-
 'use client';
-
-import Slider from "@madzadev/image-slider";
-import "@madzadev/image-slider/dist/index.css";
-
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 
-const Images = [
+
+const slideImages = [
   {
     url: '/assets/about/background.jpeg',
     caption: 'Solutions For Better Living 1',
     desc: '',
   },
   {
-    url: '/assets/about/background.jpg',
+    url: '/assets/about/background.jpeg',
     caption: 'Solutions For Better Living 2',
     desc: '',
   },
@@ -25,29 +22,40 @@ const Images = [
   },
 ];
 
-
 const Slideshow = () => {
   return (
     <section
-      id="home"
-      className="
-    ">
-      <div className=" w-full h-full bg-cover bg-no-repea">
-        <Slider
-          loop={true}
-          autoPlay={true}
-          autoPlayInterval={3000} // in milliseconds
-          showArrowControls={false}
-          showDotControls={true}
-          
-          
-        imageList={Images} height={500} width='100%'
-        />
+      className="mb-10"
+    >
+      <Slide 
+      arrows={false} 
+      canSwipe={true} 
+      infinite={true} 
+      duration={3000}  
+      transitionDuration={2000}
+      
+      >
+        {slideImages.map((slideImage, index) => (
+          <div key={index}>
+            <div className="bg-cover bg-no-repeat" style={{ 'backgroundImage': `url(${slideImage.url})`, height: '75vh', width:'100%'}}>
 
-          <div className='mask h-full flex items-center justify-center ' style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+              {/**The shadow */}
+              <div className='mask h-full flex items-center justify-center' style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                <h1 className='b-5 text-3xl font-bold leading-tight text-white dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight tracking-wider flex '>
+                  {slideImage.caption}
+                </h1>
+
+                <p className='white-space: pre-line'>
+                  {slideImage.desc}
+                </p>
+
+              </div>
+
+            </div>
           </div>
 
-      </div>
+        ))}
+      </Slide>
     </section>
   )
 }
