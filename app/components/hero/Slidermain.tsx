8ef1fef580cf5561372029ea44c4c8ddebcdd66b
ramzi <ramzi_maresh@outlook..com>
2024-03-23 17:03:57 +0300

    qq

--- app/components/hero/Slidermain.tsx
+++ app/components/hero/Slidermain.tsx
@@ -7,36 +7,47 @@ import 'react-slideshow-image/dist/styles.css'
 const slideImages = [
   {
     url: '/assets/about/background.jpeg',
-    caption: 'Solutions For Better Living'
+    caption: 'Solutions For Better Living 1',
+    desc: '',
   },
   {
     url: '/assets/about/slidertwo.png',
-    caption: ''
+    caption: 'Solutions For Better Living 2',
+    desc: '',
   },
   {
     url: '/assets/about/background.jpeg',
-    caption: ''
+    caption: 'Solutions For Better Living 3',
+    desc: '',
   },
 ];
 
 const Slideshow = () => {
-    return (
-      <section 
+  return (
+    <section
       className="mb-10"
-      >
-        <Slide arrows={false} canSwipe={true} infinite={true} >
-         {slideImages.map((slideImage, index)=> (
-            <div key={index}>
-              <div className="bg-cover bg-no-repeat w-full h-full bg-custom-image h-full" style={{'backgroundImage': `url(${slideImage.url})`, height : 550 }}>
+    >
+      <Slide arrows={false} canSwipe={true} infinite={true} duration={3000}  >
+        {slideImages.map((slideImage, index) => (
+          <div key={index}>
+            <div className="bg-cover bg-no-repeat w-full h-full" style={{ 'backgroundImage': `url(${slideImage.url})`, height: '600px' }}>
               <div className='mask h-full flex items-center justify-center ' style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-                <h1 className='b-5 text-3xl font-bold leading-tight text-white dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight tracking-wider'>{slideImage.caption}</h1>
-            </div>
-            </div>
-            </div>
+                <h1 className='b-5 text-3xl font-bold leading-tight text-white dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight tracking-wider flex '>
+                  {slideImage.caption}
+                </h1>
 
-          ))} 
-        </Slide>
-      </section>
-    )
+                <p className='white-space: pre-line'>
+                  {slideImage.desc}
+                </p>
+                
+              </div>
+
+            </div>
+          </div>
+
+        ))}
+      </Slide>
+    </section>
+  )
 }
 export default Slideshow
